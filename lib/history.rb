b02489873fbeafd450f4983e628513988ce6e6cb
geoffreyweal <geoffrey.weal@gmail.com>
2026-06-01 18:01:24 +1200

    update version of the NeSI OOD OpenComposer App

--- lib/history.rb
+++ lib/history.rb
@@ -1288,6 +1288,7 @@ helpers do
     filter_text = CGI.unescapeHTML(filter.to_s).downcase
 
     all_ids.filter_map do |job_id|
+      next if job_id.to_s.match?(/\A\d+_\[/)
       next if deleted_generic.include?(job_id) && db_map[job_id].nil?
       row = build_combined_row(job_id, sacct_map[job_id], db_map[job_id])
 

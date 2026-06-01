4dfd588ec2daffa04b7c95ba73163eb75e91c88b
geoffreyweal <geoffrey.weal@gmail.com>
2026-06-01 18:24:30 +1200

    update version of the NeSI OOD OpenComposer App

--- lib/history.rb
+++ lib/history.rb
@@ -1057,7 +1057,7 @@ helpers do
 
       existing = job_record_to_internal_hash(record)
       scheduler_data = {
-        "_status" => JOB_STATUS["completed"]
+        "_status" => JOB_STATUS["cancelled"]
       }
 
       upsert_job(
@@ -1107,20 +1107,6 @@ helpers do
             existing: existing,
             submit_data: nil,
             scheduler_data: scheduler_data
-          )
-        )
-      end
-
-      (queried_ids - status.keys).each do |id|
-        record = find_job(db, id)
-        next unless record
-        existing = job_record_to_internal_hash(record)
-        upsert_job(
-          db,
-          build_job_record(
-            existing: existing,
-            submit_data: nil,
-            scheduler_data: { "_status" => JOB_STATUS["cancelled"] }
           )
         )
       end
